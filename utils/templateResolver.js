fab40c442281742e48c1490b4773111564dca16d
Adham Sherif <adhamsherif7261@gmail.com>
2026-01-18 02:36:39 +0200

    Added Mapings To The Remaining Doc Types

--- utils/templateResolver.js
+++ utils/templateResolver.js
@@ -56,6 +56,14 @@ function mapDocTypeToTemplateKey(docType) {
     // future
     'Domestic Organic Pesticide': 'organic_pesticide',
     'Imported Organic Pesticide': 'organic_pesticide',
+    'Organic Farm' : 'organic_farm',
+    'Exporters Organic Production' : 'exporters_organic_production',
+    'Importers Organic Production' : 'importers_organic_production',
+    'Warehouse' : 'warehouse',
+    'Factory Or Production Unit' : 'factory_or_production_unit',
+    'Conformity Office Or Entity' : 'conformity_office_or_entity',
+    'Consultancy Firms Or Scientific Offices' : 'consultancy_firms_or_scientific_offices',
+    'Organic Feed Logo' : 'organic_feed_logo',
   };
 
   const key = mapping[docType];
